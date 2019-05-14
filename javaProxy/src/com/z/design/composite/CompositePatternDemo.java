c1ab22bd941bb979f7fb081dedc7212d42ab0399
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2019-05-14 11:46:00 +0800

    组合模式

---
+++ javaProxy/src/com/z/design/composite/CompositePatternDemo.java
@@ -0,0 +1,48 @@
+package com.z.design.composite;
+
+/**
+ * User: zhangkb
+ * Date: 2019/5/14 0014
+ * Time: 上午 11:34
+ * 组合模式（Composite Pattern），又叫部分整体模式，是用于把一组相似的对象当作一个单一的对象。
+ * 组合模式依据树形结构来组合对象，用来表示部分以及整体层次。这种类型的设计模式属于结构型模式，它创建了对象组的树形结构。
+ *
+ * 这种模式创建了一个包含自己对象组的类。该类提供了修改相同对象组的方式。
+ *
+ * 我们通过下面的实例来演示组合模式的用法。实例演示了一个组织中员工的层次结构
+ * 意图：将对象组合成树形结构以表示"部分-整体"的层次结构。组合模式使得用户对单个对象和组合对象的使用具有一致性
+ * 比如：老板管三个经理，三个经理分别管十个人
+ */
+public class CompositePatternDemo {
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //打印该组织的所有员工
+        System.out.println(CEO);
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
---
+++ javaProxy/src/com/z/design/composite/Employee.java
@@ -0,0 +1,43 @@
+package com.z.design.composite;
+
+/**
+ * User: zhangkb
+ * Date: 2019/5/14 0014
+ * Time: 上午 11:34
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    @Override
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
