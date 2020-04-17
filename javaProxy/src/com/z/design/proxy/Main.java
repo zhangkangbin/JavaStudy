ea6b1f2e4661aa1e59bd31a3ba518cb0234c49a1
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2020-04-17 14:51:17 +0800

    动态代理生成原理

--- javaProxy/src/com/z/design/proxy/Main.java
+++ javaProxy/src/com/z/design/proxy/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-       System.out.println("------------静态代理------------");
+        System.out.println("------------静态代理------------");
         //静态代理
         IUser myProxy = new MyProxy(new My());
         //MyProxy 代理 new My()
@@ -30,30 +30,29 @@ public class Main {
 
         //动态代理
         IUser my = new My();
+
+        //源码里会把我们生成的class 放在proxyClassCache 缓存起来 ，反射调取方法
         IUser proxySubject = (IUser) Proxy.newProxyInstance(my.getClass().getClassLoader(),
                 my.getClass().getInterfaces(),
                 new MyProxyHandler<>(my));
         proxySubject.findGirl();
 
 
-
-        //模拟生成一个class
+        /**
+         * {@link (testCreate$ProxyClass)}
+         * #模拟生成一个class ，参考 @ testCreate$ProxyClass ，这个类是反编译后复制到这里的。
+         */
         ProxyUtil.createClass(my.getClass().getInterfaces());
-
-
-
 
 
         //这个跟动态代理是一样的。
         System.out.println("------------匿名代理------------");
 
-        ProxyUtil proxyUtil=new ProxyUtil();
-        proxyUtil.<IUser>getProxy(IUser.class,new My()).findGirl();
-
+        ProxyUtil proxyUtil = new ProxyUtil();
+        proxyUtil.<IUser>getProxy(IUser.class, new My()).findGirl();
 
 
     }
-
 
 
 }
---
+++ javaProxy/src/com/z/design/proxy/testCreate$ProxyClass.java
@@ -0,0 +1,77 @@
+package com.z.design.proxy;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import com.z.design.proxy.IUser;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.UndeclaredThrowableException;
+
+public final class testCreate$ProxyClass extends Proxy implements IUser {
+    private static Method m1;
+    private static Method m2;
+    private static Method m3;
+    private static Method m0;
+
+    public testCreate$ProxyClass(InvocationHandler var1) throws  {
+        super(var1);
+    }
+
+    public final boolean equals(Object var1) throws  {
+        try {
+            return (Boolean)super.h.invoke(this, m1, new Object[]{var1});
+        } catch (RuntimeException | Error var3) {
+            throw var3;
+        } catch (Throwable var4) {
+            throw new UndeclaredThrowableException(var4);
+        }
+    }
+
+    public final String toString() throws  {
+        try {
+            return (String)super.h.invoke(this, m2, (Object[])null);
+        } catch (RuntimeException | Error var2) {
+            throw var2;
+        } catch (Throwable var3) {
+            throw new UndeclaredThrowableException(var3);
+        }
+    }
+
+    public final void findGirl() throws  {
+        try {
+            //反射调取方法
+            super.h.invoke(this, m3, (Object[])null);
+        } catch (RuntimeException | Error var2) {
+            throw var2;
+        } catch (Throwable var3) {
+            throw new UndeclaredThrowableException(var3);
+        }
+    }
+
+    public final int hashCode() throws  {
+        try {
+            return (Integer)super.h.invoke(this, m0, (Object[])null);
+        } catch (RuntimeException | Error var2) {
+            throw var2;
+        } catch (Throwable var3) {
+            throw new UndeclaredThrowableException(var3);
+        }
+    }
+
+    static {
+        try {
+            m1 = Class.forName("java.lang.Object").getMethod("equals", Class.forName("java.lang.Object"));
+            m2 = Class.forName("java.lang.Object").getMethod("toString");
+            m3 = Class.forName("com.z.design.proxy.IUser").getMethod("findGirl");
+            m0 = Class.forName("java.lang.Object").getMethod("hashCode");
+        } catch (NoSuchMethodException var2) {
+            throw new NoSuchMethodError(var2.getMessage());
+        } catch (ClassNotFoundException var3) {
+            throw new NoClassDefFoundError(var3.getMessage());
+        }
+    }
+}
