145002dfce60a42c64322072f9217dbc0ce9a3ba
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2020-11-11 16:25:12 +0800

    kmp 算法

--- javaProxy/src/com/z/algorithm/KmpTest.java
+++ javaProxy/src/com/z/algorithm/KmpTest.java
@@ -6,10 +6,15 @@ package com.z.algorithm;
  */
 public class KmpTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        String[] match={"a","b","c","d","e"};
+       // getNext(match);
+        kmpFind();
+        //bfFind();
 
-        bfFind();
+      //  char[] chars={1,2,1,2};
+    //    GetNext(chars);
     }
 
     /**
@@ -18,7 +23,9 @@ public class KmpTest {
      * @return
      */
     private static  int  bfFind(){
-        String[] strings={"a","b","c","d","e","z","f"};
+        //i
+        String[] strings={"a","d","e","c","d","e","z","f"};
+        //j
         String[] match={"d","e","z"};
 
         int i=0;
@@ -38,6 +45,7 @@ public class KmpTest {
         }
 
         if(j==match.length){
+            //打印而已
             int start=i-match.length;
             System.out.println("找到");
             for(int stringIndex=start;stringIndex<match.length+start;stringIndex++){
@@ -50,4 +58,98 @@ public class KmpTest {
         }
 
     }
+
+
+    private static  void kmpFind() throws InterruptedException {
+        //i
+        String[] strings={"a","a","a","a","a","a","c","a","b","a","a","a","b","a"};
+        //j
+          String[] match={"a","a","a","b","a"};//j
+        //String[] match={"a","b","b","c","c"}; //-1_0_0_0_0_0
+        int i=0,j=0;
+        int[] next = getNext(match);
+        while(i<strings.length&&j<match.length){
+
+            if(j!=-1){
+                System.out.println("-----strings[i]:"+strings[i]);
+                System.out.println("-----strings[j]:"+match[j]+"\n");
+            }
+            if(j==-1||strings[i].equals(match[j])){
+                i++;
+                j++;
+            }else {
+                //match 回到 j
+                j=next[j];
+                System.out.println("j:"+j);
+            }
+        }
+
+        if(j==match.length){
+            //打印而已
+            int start=i-match.length;
+            System.out.println("找到");
+
+        }else {
+            System.out.println("没找到");
+
+        }
+
+
+    }
+
+    private static int[]  getNext(String[] strings){
+
+        int[] next=new int[strings.length+1];
+
+        int i=0,j=-1;
+        next[0]=j;//-1
+        // {"a","b","a","b","b"};
+        //后挪一位，比较相同就加一。
+        while (i<strings.length){
+            if(j==-1||strings[i].equals(strings[j])){
+
+                //j=-1,所以 下标1 一定是0,-1++=0;
+                i++;
+                j++;
+
+                next[i]=j;
+
+            }else {
+                j=next[j];
+            }
+
+        }
+
+
+        for (int k : next) {
+            System.out.print("_" + k);
+        }
+        System.out.println("");
+        
+        return next;
+
+    }
+
+
+    static void GetNext(char T[])
+    {
+        int[] next=new int[T.length+1];
+        int j=0,k=-1;
+        next[j]=k;
+        while(j<T.length)
+        {
+            if(k==-1||T[j]==T[k])
+            {
+                j++;
+                k++;
+                next[j]=k;
+            }
+            else k=next[k];
+        }
+
+        for (int s : next) {
+            System.out.print(" " + s);
+        }
+    }
+
 }
