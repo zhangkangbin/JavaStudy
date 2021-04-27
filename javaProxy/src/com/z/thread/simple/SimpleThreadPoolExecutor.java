f3e4e23e283471d3b85dcb8fe916d2a240d51b3d
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2021-04-27 12:45:50 +0800

    简单模拟线程池

--- javaProxy/src/com/z/thread/simple/SimpleThreadPoolExecutor.java
+++ javaProxy/src/com/z/thread/simple/SimpleThreadPoolExecutor.java
@@ -1,6 +1,5 @@
 package com.z.thread.simple;
 
-import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -8,9 +7,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * 受volley源码的影响
  * 简单模拟线程池
- * ThreadPoolExecutor 的 allowCoreThreadTimeOut 默认也是不回收的，可见占用的系统资源很少，应该还没有你频繁创建线程的和回收消耗。
- * 因为通常将处于阻塞状态的进程排成一个队列， 称为阻塞队列。在有的系统中，
- * 按阻塞的原因不同而将处于阻塞状态的进程排成多个队列。
+ *
  * todo:备注:任务限制，拒绝策略。
  */
 public class SimpleThreadPoolExecutor {
@@ -22,11 +19,11 @@ public class SimpleThreadPoolExecutor {
      */
     private final BlockingQueue<Task> workQueue;
 
-    //ThreadPoolExecutor中是用AtomicInteger，代表线程状态和在工作线程数 ,简单起见就不了。
+    //ThreadPoolExecutor中是用AtomicInteger，代表线程状态和在工作线程数 ,简单起见就不了。待我再写一篇文章吧！这个状态有点意思。
     private final AtomicInteger threadCount = new AtomicInteger(0);
 
     public SimpleThreadPoolExecutor() {
-        //任务上限30超过，抛出异常。
+        //任务上限30，超过抛出异常。
         workQueue = new ArrayBlockingQueue<Task>(30);
     }
 
@@ -37,13 +34,17 @@ public class SimpleThreadPoolExecutor {
     }
 
     /**
-     * 开始处理任务
+     * 新建线程和添加任务
      * @param task
      */
     public void start(Task task) {
 
         //启动多个线程去取任务。核心也在这里。
         if (threadCount.get() < threadSize) {
+            // ThreadPoolExecutor 的 allowCoreThreadTimeOut 默认也是不回收的，
+            // 可见占用的系统资源很少，应该还没有你频繁创建线程的和回收消耗。
+            //因为通常将处于阻塞状态的进程排成一个队列， 称为阻塞队列。在有的系统中，
+            //按阻塞的原因不同而将处于阻塞状态的进程排成多个队列。
             int count = threadCount.incrementAndGet();
             System.out.println("count：" + count);
             startThread(count);
@@ -59,7 +60,7 @@ public class SimpleThreadPoolExecutor {
 
     /**
      * 模拟处理任务。
-     *
+     *process task
      * @param nameTag
      */
     private void startThread(int nameTag) {
@@ -93,7 +94,7 @@ public class SimpleThreadPoolExecutor {
 
     /**
      * 移除任务
-     *
+     *remove task
      * @param obj 任务
      */
     public void removeTask(Task obj) {
@@ -102,6 +103,7 @@ public class SimpleThreadPoolExecutor {
 
     /**
      * 清除所有任务
+     * clear all tasks
      */
     public void clearAllTask() {
         workQueue.clear();
