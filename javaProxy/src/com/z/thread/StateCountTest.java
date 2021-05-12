69ee3e1c10dd582842dc13986693b2adf651fa7f
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2021-05-12 11:39:45 +0800

    线程池状态值修改实践

--- javaProxy/src/com/z/thread/StateCountTest.java
+++ javaProxy/src/com/z/thread/StateCountTest.java
@@ -1,28 +1,25 @@
 package com.z.thread;
 
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * 我们用一个值保存两个数，
  */
 public class StateCountTest {
-    //  private static final int COUNT_BITS = Integer.SIZE - 3;
     //一半16用来存储高位，一半存低位，（神奇的存储方式。减少变量，减少锁的竞争）
     //一半高3位用来存储状态，低29位存 线程池线程运行数量，（神奇的存储方式。减少变量，减少锁的竞争）
-    private static final int COUNT_BITS = 29;//32-3=29
+    private static final int COUNT_BITS = 29;//Integer.SIZE
     //看来这个值CAPACITY不是随便写的，他的二进制，都是29位的1，1111111111111111，如果COUNT_BITS是16，就是低位补160，减一个1，结果就是16个1，如果是2，就是2个1。
     // 。(1 << SHARED_SHIFT) - 1;
     private static final int CAPACITY = (1 << COUNT_BITS) -1;
-
-    private static final int RUNNING    = -1 << COUNT_BITS;
-    private static final int SHUTDOWN   =  0 << COUNT_BITS;
-
+    private static final int DEFAULT    = -1 << COUNT_BITS;
+    private static final int RUNNING   =  0 << COUNT_BITS;
     private static final int STOP       =  1 << COUNT_BITS;
-    private static final int TIDYING    =  2 << COUNT_BITS;
+    private static AtomicInteger ctl=new AtomicInteger(ctlOf(DEFAULT, 0));
 
-
-    // Packing and unpacking ctl
+    private static int ctlOf(int rs, int wc) { return rs | wc; }
     /**
      *减少锁的竞争。
      * &与运算
@@ -33,38 +30,73 @@ public class StateCountTest {
      */
     private static int runStateOf(int c) {
 
-
-        //~即0变成1，1变成0.(简单来说是加一，然后取反)比如 3 ，加一，是4，取反为-4.
-        //CAPACITY =536870911，~后是-536870912
-        //CAPACITY 取反进制是 11100000000000000000000000000000
-        //RUNNING  的取进制是 11100000000000000000000000000000
         //todo:敲重点,所以这里是取的高位
         return c & ~CAPACITY;
     }
 
     private static int workerCountOf(int c) {
-        //CAPACITY 536870911 进制是 00011111111111111111111111111111
-        //-536870912 加一 进制是     11100000000000000000000000000001
-        //-536870912 加二 进制是     11100000000000000000000000000010
-        //& 两个操作数中，位都为1，结果才为1，否则结果为0
+
         //todo:敲重点,所以这里是取的是低位。
         return c & CAPACITY;
     }
 
+    /**
+     * 增加线程
+     * @param expect 预期值
+     * @return 是否添加成功。
+     */
+    private static boolean incrementWorkerCount(int expect) {
+
+        return ctl.compareAndSet(expect, expect + 1);
+    }
+
+
+    /**
+     * 减少线程
+     * @param expect 预期值
+     * @return 是否成功。
+     */
+    private static boolean decrementWorkerCount(int expect) {
+
+        return ctl.compareAndSet(expect, expect -1);
+    }
+
+    /**
+     * 修改状态
+     * @param state
+     * @return
+     */
+    private static boolean changeState(int state) {
+
+        //修改状态值为stop
+        return ctl.compareAndSet(ctl.get(), ctlOf(state, 0));
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(runStateOf(RUNNING));
-        System.out.println(runStateOf(SHUTDOWN));
-        System.out.println(STOP);
-        System.out.println(-2 << COUNT_BITS);
-        System.out.println(runStateOf(TIDYING));
+        System.out.printf("当前状态：%s \n",runStateOf(ctl.get()));
 
 
-        System.out.println(workerCountOf(RUNNING));
-        System.out.println(workerCountOf(RUNNING+1));
-        System.out.println(workerCountOf(RUNNING+2));
+        changeState(STOP);
+        System.out.printf("当前状态：%s \n",runStateOf(ctl.get()));
+
+        //修改状态值为RUNNING
+        changeState(RUNNING);
+        System.out.printf("当前状态：%s \n",runStateOf(ctl.get()));
+
+        //添加一个线程
+        System.out.println("增加一个线程，是否成功："+incrementWorkerCount(ctl.get()));
+        //查看线程数
+        System.out.println(workerCountOf(ctl.get()));
+
+        System.out.println("增加一个线程，是否成功："+incrementWorkerCount(ctl.get()));
+        //查看线程数
+        System.out.println(workerCountOf(ctl.get()));
 
 
+        System.out.println("减少一个线程，是否成功："+decrementWorkerCount(ctl.get()));
+        //查看线程数
+        System.out.println(workerCountOf(ctl.get()));
     }
 
 }
